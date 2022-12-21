d6819a209cec6a8f76ec740a5ecb3c50c9bcbc7d
markovojinovic <vojinomarko@gmail.com>
2022-12-21 15:01:40 +0100

    Initial commit

---
+++ algorithms.py
@@ -0,0 +1,63 @@
+class Algorithm:
+    def get_algorithm_steps(self, tiles, variables, words):
+        pass
+
+
+
+class ExampleAlgorithm(Algorithm):
+
+    def get_algorithm_steps(self, tiles, variables, words):
+        moves_list = [['0h', 0], ['0v', 2], ['1v', 1], ['2h', 1], ['4h', None],
+                 ['2h', None], ['1v', None], ['0v', 3], ['1v', 1], ['2h', 1],
+                 ['4h', 4], ['5v', 5]]
+        domains = {var: [word for word in words] for var in variables}
+        solution = []
+        for move in moves_list:
+            solution.append([move[0], move[1], domains])
+        return solution
+
+
+
+class Backtracking(Algorithm):
+
+    def get_algorithm_steps(self, tiles, variables, words):
+
+        moves = []
+        for var in variables:
+            leng = variables[var]
+
+            noEntr = False
+            for word in words:
+                if len(word) == leng:
+                    moves.append([var, leng])
+                    noEntr = True
+
+            if noEntr:
+                noEntr = False
+
+
+class ForwardChecking(Algorithm):
+
+    def get_algorithm_steps(self, tiles, variables, words):
+        moves_list = [['0h', 0], ['0v', 2], ['1v', 1], ['2h', 1], ['4h', None],
+                 ['2h', None], ['1v', None], ['0v', 3], ['1v', 1], ['2h', 1],
+                 ['4h', 4], ['5v', 5]]
+        domains = {var: [word for word in words] for var in variables}
+        solution = []
+        for move in moves_list:
+            solution.append([move[0], move[1], domains])
+        return solution
+
+
+
+class ArcConsistency(Algorithm):
+
+    def get_algorithm_steps(self, tiles, variables, words):
+        moves_list = [['0h', 0], ['0v', 2], ['1v', 1], ['2h', 1], ['4h', None],
+                 ['2h', None], ['1v', None], ['0v', 3], ['1v', 1], ['2h', 1],
+                 ['4h', 4], ['5v', 5]]
+        domains = {var: [word for word in words] for var in variables}
+        solution = []
+        for move in moves_list:
+            solution.append([move[0], move[1], domains])
+        return solution
